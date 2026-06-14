0fb51580a7feee09ceebf5cac720b8230b65dd11
pan-vodnik <pan_vodnik@email.cz>
2026-06-14 13:16:30 +0200

    disable local logging

--- log.js
+++ log.js
@@ -22,32 +22,34 @@ let id =
 
 localStorage.setItem("pribehy2_uid", id);
 
-ipCache.then((ip) => {
-  fetch("https://api.logsnag.com/v1/log", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer 04589ea10e64a3fe880032cdc5be7c06",
-    },
-    body: JSON.stringify({
-      project: "pribehy2",
-      channel: "visits",
-      event: "Visit",
-      notify: true,
-      user_id: localStorage.getItem("pribehy2_uid"),
-      tags: {
-        ip: ip.ip,
-        location: ip.country + "/" + ip.city,
-        pwa: pwa,
-        local: local,
-        referrer: document.referrer,
-        userAgent: navigator.userAgent,
-        screen: `${window.screen.width}x${window.screen.height}`,
-        language: window.navigator.language,
+if (!local) {
+  ipCache.then((ip) => {
+    fetch("https://api.logsnag.com/v1/log", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer 04589ea10e64a3fe880032cdc5be7c06",
       },
-      description: `User \`${localStorage.getItem("pribehy2_uid")}\` opened the app.`,
-      parser: "markdown",
-      icon: "👁 ",
-    }),
+      body: JSON.stringify({
+        project: "pribehy2",
+        channel: "visits",
+        event: "Visit",
+        notify: true,
+        user_id: localStorage.getItem("pribehy2_uid"),
+        tags: {
+          ip: ip.ip,
+          location: ip.country + "/" + ip.city,
+          pwa: pwa,
+          local: local,
+          referrer: document.referrer,
+          userAgent: navigator.userAgent,
+          screen: `${window.screen.width}x${window.screen.height}`,
+          language: window.navigator.language,
+        },
+        description: `User \`${localStorage.getItem("pribehy2_uid")}\` opened the app.`,
+        parser: "markdown",
+        icon: "👁 ",
+      }),
+    });
   });
-});
+}
